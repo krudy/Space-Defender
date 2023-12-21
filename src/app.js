862b2be4056d38a153ac050493ce5c5169d88ae5
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2023-12-21 12:30:20 +0100

    bullets added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 const playerElement = document.querySelector('#player');
 const gameBoardElement = document.querySelector('#game-board');
 
+const bullets = [];
+
 const movePlayer = (direction) => {
     // calculation of player's new position
     const newPosition = playerElement.offsetLeft + direction * 10;
@@ -17,12 +19,46 @@ const movePlayer = (direction) => {
     }
 }
 
+const createBullet = () => {
+    // bullet definition
+    const bullet = document.createElement('div');
+    bullet.className = 'bullet';
+    bullet.style.left = `${playerElement.offsetLeft}px`;
+    bullet.style.bottom = `${playerElement.offsetHeight}px`;
+
+    // adding bullet to the gameboard area
+    gameBoardElement.appendChild(bullet);
+    bullets.push(bullet);
+
+}
+
 const handleKeyboard = (event) => {
     switch(event.code) {
         case 'ArrowLeft':  {movePlayer(-1); break;}
-        case 'ArrowRight': {movePlayer(1);}
+        case 'ArrowRight': {movePlayer(1); break;}
+        case 'Space': createBullet();
+
     }
 }
 
 //handling the keyboard
 window.addEventListener('keydown', handleKeyboard);
+
+const moveBullets = () => {
+    for(let i = 0; i < bullets.length; i++){
+       const bullet = bullets[i];
+       
+       // moving bullet
+       bullet.style.top = `${bullet.offsetTop - 10}px`
+       
+       // removing bullet if it is off the board
+       if(bullet.offsetTop < 0 ){
+           bullets.splice(i,1);
+           i--;
+           bullet.remove();
+        }
+    }
+}
+
+//intervals
+setInterval(moveBullets, 50);
